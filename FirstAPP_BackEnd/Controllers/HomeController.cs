e0c6488f727eef6c9e782556cab45f946b579a5f
Malik8008 <tu60prbs1@code.edu.az>
2022-05-23 12:09:09 +0400

    BackEnd Intro completed

---
+++ FirstAPP_BackEnd/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPP_BackEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(int id)
+        {
+            ViewBag.Product = "Milk";
+            ViewData["Count"] = 5;
+            TempData["Quality"] = "Medium";
+            return RedirectToAction("About");
+           return View();
+            
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult FAQ(string name,string surname)
+        {
+            string fullname = $"{name} {surname}";
+            return Content(fullname);
+        }
+    }
+}
